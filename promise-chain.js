6ec781263eb6b3824ade5b25f259c945d860ef84
Karen Huang <thinknfix2021@gmail.com>
2025-03-11 00:02:53 +0800

    add: async,await

--- promise-chain.js
+++ promise-chain.js
@@ -5,10 +5,11 @@ function correctTest(name) {
     setTimeout(() => {
       const score = Math.round(Math.random() * 100);
       const data = { name, score };
+      console.log('成績出爐:', score);
       if (score >= 20) {
         resolve(data);
       } else {
-        reject("您已被退學！");
+        reject({score, msg:"掰掰，您已被退學！"});
       }
     }, 2000);
   });
@@ -24,7 +25,7 @@ function checkReward(data) {
       } else if (data.score >= 60 && data.score < 90) {
         resolve({ gift: "吉伊卡哇貼紙乙張", score: data.score });
       } else {
-        reject({score: data.score, msg:"即將受到毒打一頓！"});
+        reject({ score: data.score, msg: "即將受到毒打一頓！" });
       }
     }, 2000);
   });
@@ -38,3 +39,19 @@ correctTest("Ming")
   })
   .then((reward) => console.log("獎品:", reward.gift))
   .catch((error) => console.log(`分數${error.score}`, error.msg)); //catch可以接收兩個promise之中任一的錯誤
+
+//async、await in promise chain (可讀性更高)
+const init = async function () {
+  try {
+    //暫停等待回傳
+    const studentA = await correctTest("Billy"); //then回傳的到studentA
+    console.log("studentA", studentA);
+    //兩秒後執行下一段語法
+    const rewardA = await checkReward(studentA);
+    console.log("rewardA", rewardA);
+  } catch (error) {
+    console.log("error", error.msg);
+  }
+};
+
+init();
